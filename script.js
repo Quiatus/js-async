c72cf4668b7b27a02665e44cbc3e40fb1fb931d5
Quiatus <Quiatus@gmail.com>
2024-06-11 22:16:26 +0200

    add error handling

--- script.js
+++ script.js
@@ -19,33 +19,70 @@ const renderCountry = (data, className = '') => {
         </article>`
     
         countriesContainer.insertAdjacentHTML('beforeend', html)
-        countriesContainer.style.opacity = 1
+}
+
+const renderError = (msg) => {
+    countriesContainer.insertAdjacentText('beforeend', msg)
 }
 
 // old way
-const getCountryData = (country) => {
-    const req = new XMLHttpRequest()
-    req.open('GET', `https://countries-api-836d.onrender.com/countries/name/${country}`)
-    req.send()
+// const getCountryData = (country) => {
+//     const req = new XMLHttpRequest()
+//     req.open('GET', `https://countries-api-836d.onrender.com/countries/name/${country}`)
+//     req.send()
     
-    req.addEventListener('load', function() {
-        const [res] = JSON.parse(this.responseText)
-        renderCountry(res)
-        // const neighbour = res.borders
+//     req.addEventListener('load', function() {
+//         const [res] = JSON.parse(this.responseText)
+//         renderCountry(res)
+//         // const neighbour = res.borders
 
-        // if (!neighbour) return
+//         // if (!neighbour) return
 
-        // neighbour.forEach(country => {
-        //     const req2 = new XMLHttpRequest()
-        //     req2.open('GET', `https://countries-api-836d.onrender.com/countries/alpha/${country}`)
-        //     req2.send()
+//         // neighbour.forEach(country => {
+//         //     const req2 = new XMLHttpRequest()
+//         //     req2.open('GET', `https://countries-api-836d.onrender.com/countries/alpha/${country}`)
+//         //     req2.send()
 
-        //     req2.addEventListener('load', function() {
-        //         const res2 = JSON.parse(this.responseText)
-        //         renderCountry(res2, 'neighbour')
-        //     })
-        // })
+//         //     req2.addEventListener('load', function() {
+//         //         const res2 = JSON.parse(this.responseText)
+//         //         renderCountry(res2, 'neighbour')
+//         //     })
+//         // })
+//     })
+// }
+
+// getCountryData('germany')
+
+
+// promises + chaining promises (then)
+
+const url = 'https://countries-api-836d.onrender.com/countries/name/'
+const nburl = 'https://countries-api-836d.onrender.com/countries/alpha/'
+
+const getJSON = (url, errorMsg = 'Something went wrong') => {
+    return fetch(url).then(res => {
+        if (!res.ok) throw new Error(errorMsg)
+        return res.json()
     })
 }
 
-getCountryData('germany')
+const getCountryData = (country) => {
+    getJSON(url+country, 'Country not found')
+        .then((data) => {
+            renderCountry(data[0])
+            const nb = data[0].borders?.[0]
+            return getJSON(nburl + nb, 'No neighbouring country found')
+        })
+        .then((data) => renderCountry(data, 'neighbour'))
+        .catch(err => renderError(err.message))
+        .finally(() => {
+            btn.textContent = 'Where am I?'
+            countriesContainer.style.opacity = 1
+        })
+}
+
+btn.addEventListener('click', () => {
+    countriesContainer.innerHTML = ''
+    btn.textContent = 'Loading...'
+    getCountryData('netherlands')
+})
